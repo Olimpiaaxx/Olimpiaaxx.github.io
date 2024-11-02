af8d8dbc96abbcad44dba722a13c07b33891d5df
olimpia <olimpiapater@gmail.com>
2024-11-02 22:07:40 +0000

    checking

--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { App } from './App';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { HashRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import { App } from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-   </HashRouter>
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
